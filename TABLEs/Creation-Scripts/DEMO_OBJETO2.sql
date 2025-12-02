3001b27b514d136ddc67cf80370748d1c11ddadf
lsaperi <lsaperi>
2025-12-02 18:37:54 +0000

    fdfdsfds

---
+++ TABLEs/Creation-Scripts/DEMO_OBJETO2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE DEMO_OBJETO2
+   (	ID NUMBER,
+	DESCRIPCION VARCHAR2(100),
+	FECHA_CREACION DATE
+   ) ;
+-- COMMENT --
+
+   COMMENT ON TABLE DEMO_OBJETO2  IS 'Tabla de demostracigfgfdgón para pruebas de comentarios';
