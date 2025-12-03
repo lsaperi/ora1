a5c036fc70ba207026c75bf3e54a3453148114c3
lsaperi <lsaperi>
2025-12-03 13:40:32 +0000

    fdsfdsfdsfds

---
+++ TYPEs/Creation-Scripts/TYPE_B.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE EDITIONABLE TYPE TYPE_B AS OBJECT (
+    field_b NUMBER,
+    a_col   type_a
+);
+/
+
---
+++ TYPEs/Creation-Scripts/TYPE_C.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE EDITIONABLE TYPE TYPE_C AS OBJECT (
+    field_c DATE,
+    b_col   type_b
+);
+/
+
